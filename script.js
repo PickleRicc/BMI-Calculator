27cafe52e0664a866cf4cc02dca86f7e8d021f1d
Eric Harrison <107382439+PickleRicc@users.noreply.github.com>
2023-08-05 08:43:10 -0400

    New

--- script.js
+++ script.js
@@ -9,22 +9,94 @@ options.forEach((option) => {
 
 const metric = document.getElementById('metric');
 const imperial = document.getElementById('imperial');
-const imperialhidden = document.getElementById('input-hidden');
-const submitBtn = document.getElementById('submit-form')
+const imperialhidden = document.getElementById('height-in');
+const submitBtn = document.getElementById('submit-Btn')
+const bmiEl = document.getElementById('bmi-el')
+const labelCm = document.getElementById('label-cm');
+const labelKg = document.getElementById('label-kg');
+const heightCm = document.getElementById('height-cm');
+const weightKg = document.getElementById('weight-kg');
+const bmiForm = document.getElementById('bmi-form');
+
+
+heightCm.addEventListener('focus', function() {
+  this.placeholder = '';
+});
+
+weightKg.addEventListener('focus', function() {
+  this.placeholder = '';
+});
+
+
 
 
 
 metric.addEventListener('click', function(){
     imperialhidden.style.display = 'none';
-    console.log('metric clicked')
-    console.log(heightCm);
+    labelCm.textContent = 'cm';
+    labelKg.textContent = 'kg';
+    heightCm.id = 'height-cm';
+    weightKg.id = 'weight-kg';
+    
+    
+    
+
+    
 })
 
 imperial.addEventListener('click', function(){
   imperialhidden.style.display = 'inline-block'
+  labelCm.textContent = 'ft';
+  labelKg.textContent = 'lbs';
+  heightCm.id = 'height-ft';
+  weightKg.id = 'weight-lbs';
+  document.getElementById('bmi-form').reset();
+  
+  
 })
 
+const heightFt = document.getElementById('height-ft');
+const weightLbs = document.getElementById('weight-lbs');
 
-submitBtn.addEventListener('submit', function(){
-  console.log('submitted')
-})
+
+// if (metric.checked) {
+//   bmiForm.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     const cmInput = heightCm.value;
+//     const kgInput = weightKg.value;
+//     const bmi = kgInput / (cmInput / 100 * cmInput / 100);
+//     const bmiRounded = bmi.toFixed(2);
+//     alert(`Your BMI is ${bmiRounded}`);
+//   })
+// } else if (imperial.checked) {
+//   bmiForm.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     const ftInput = heightFt.value;
+//     const lbsInput = weightLbs.value;
+//     const bmi = lbsInput / (ftInput * ftInput) * 703;
+//     const bmiRounded = bmi.toFixed(2);
+//     alert(`Your BMI is ${bmiRounded}`);
+//   })
+// }
+
+
+
+bmiForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (metric.checked) {
+    const cmInput = parseFloat(document.getElementById('height-cm').value);
+    const kgInput = parseFloat(document.getElementById('weight-kg').value);
+    const bmi = kgInput / (cmInput / 100 * cmInput / 100);
+    const bmiRounded = bmi.toFixed(2);
+    bmiEl.textContent = bmiRounded;
+  } else if (imperial.checked) {
+    const ftInput = parseFloat(document.getElementById('height-ft').value);
+    const lbsInput = parseFloat(document.getElementById('weight-lbs').value);
+    const bmi = lbsInput / (ftInput * ftInput) * 703;
+    const bmiRounded = bmi.toFixed(2);
+    alert(`Your BMI is ${bmiRounded}`);
+  }
+});
+
+
